163b464c52d48a02188565ab01868ca9183cdc75
Na Minhyeok <112960401+NaMinhyeok@users.noreply.github.com>
2024-06-18 14:25:29 +0900

    [level 2] Title: 올바른 괄호, Time: 20.33 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                stack.push(s.charAt(i));
+            }
+            if(s.charAt(i)==')'){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        answer = stack.isEmpty();
+        
+        return answer;
+    }
+}
