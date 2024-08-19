f48049e40bf71adeaaec5ca86050f1ea59988800
Na Minhyeok <112960401+NaMinhyeok@users.noreply.github.com>
2024-08-19 10:20:30 +0900

    [Silver III] Title: N과 M (5), Time: 696 ms, Memory: 53332 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N,M;
+    static int[] result;
+    static int[] arr;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        visited = new boolean[N];
+        result = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0);
+    }
+
+    private static void dfs(int depth) {
+        if(depth == M) {
+            StringBuilder sb = new StringBuilder();
+            for(int num : result) {
+                sb.append(num).append(" ");
+            }
+            System.out.println(sb);
+            return;
+        }
+        for(int i=0; i<N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                dfs(depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
