5f44cebfe2002b398b79f9605d149711bc4137ed
Na Minhyeok <112960401+NaMinhyeok@users.noreply.github.com>
2024-08-20 12:15:10 +0900

    [Silver III] Title: N과 M (8), Time: 144 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] result;
+    static int[] arr;
+    static boolean[] visited;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        visited = new boolean[N];
+        result = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0,0);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static void dfs(int idx, int depth) throws IOException {
+        if (depth == M) {
+            for (int num : result) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if(depth > 0) {
+                if(result[depth-1] > arr[i]) {
+                    continue;
+                }
+            }
+            result[depth] = arr[i];
+            dfs(i+1, depth + 1);
+        }
+    }
+}
