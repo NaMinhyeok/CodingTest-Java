94cfe4f6e8d0fda951ac7356678d9f9352327c44
NaMinhyeok <nmh9097@gmail.com>
2025-02-23 23:40:55 +0900

    3번 문제

--- 코딩테스트합격자되기/03.java
+++ 코딩테스트합격자되기/03.java
@@ -1,9 +1,13 @@
-import java.util.HashSet;
+import java.util.*;
 
-public class Solution {
-
-    public static int[] solution(int[] numbers) {
-
+class Solution {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i< numbers.length-1; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
-
 }
