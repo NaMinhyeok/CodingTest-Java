ac0f756764a329f7221d9889f38f33c6c115511f
NaMinhyeok <nmh9097@gmail.com>
2025-03-15 22:50:18 +0900

    8번 stack

--- 코딩테스트합격자되기/08.java
+++ 코딩테스트합격자되기/08.java
@@ -3,7 +3,19 @@ import java.util.ArrayDeque;
 class Solution {
 
     private boolean solution(String s) {
-
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        char[] arr = s.toCharArray();
+        for(char c: arr) {
+            if (c =='(') {
+                stack.push(c);
+            }
+            else {
+                if (stack.isEmpty() || stack.pop() == c) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 
 }
