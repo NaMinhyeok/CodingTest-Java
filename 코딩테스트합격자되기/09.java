025c07b79f7051d4c5fa8521e71e715c0004974b
NaMinhyeok <nmh9097@gmail.com>
2025-03-16 23:25:13 +0900

    9번 stack

--- 코딩테스트합격자되기/09.java
+++ 코딩테스트합격자되기/09.java
@@ -10,7 +10,18 @@ public class Solution {
 
     // 이 부분을 변경해서 실행해보세요.
     public static String solution(int decimal) {
+        Stack<Integer> stack = new Stack<>();
+        while (decimal > 0) {
+            int remainder = decimal % 2;
+            stack.push(remainder);
+            decimal /= 2;
+        }
 
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
     }
 
 }
