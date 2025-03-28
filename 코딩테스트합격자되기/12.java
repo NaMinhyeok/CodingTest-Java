0876e2856b4f54715518950a995740834b255c56
NaMinhyeok <nmh9097@gmail.com>
2025-03-28 22:42:26 +0900

    12번 stack

--- 코딩테스트합격자되기/12.java
+++ 코딩테스트합격자되기/12.java
@@ -3,7 +3,24 @@ import java.util.Stack;
 class Solution {
 
     public static int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
 
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for(int i=1; i<n; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int top = stack.pop();
+                answer[top] = i - top;
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            answer[top] = n - 1 - top;
+        }
+        return answer;
     }
 
 }
