54545e5490a946d944bb0156a5e2f6de5aaa61d3
NaMinhyeok <nmh9097@gmail.com>
2025-04-09 23:40:39 +0900

    15번 큐

--- 코딩테스트합격자되기/15.java
+++ 코딩테스트합격자되기/15.java
@@ -8,7 +8,19 @@ public class Solution {
 
     // 이 부분을 변경해서 실행해보세요.
     private static int solution(int N, int K) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i=1; i<=N; i++) {
+            deque.addLast(i);
+        }
 
+        while (deque.size() > 1) {
+            for(int i=0; i<K-1; i++) {
+                deque.addLast(deque.pollFirst());
+            }
+            deque.pollFirst();
+        }
+
+        return deque.pollFirst();
     }
 
 }
