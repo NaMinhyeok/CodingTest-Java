b9542ea637f8c0dc4331ec4317c73d760965ebf5
NaMinhyeok <nmh9097@gmail.com>
2025-04-11 20:12:02 +0900

    17번 큐

--- 코딩테스트합격자되기/17.java
+++ 코딩테스트합격자되기/17.java
@@ -4,7 +4,22 @@ import java.util.Arrays;
 public class Solution {
 
     public String solution(String[] cards1, String[] cards2, String[] goal) {
+        ArrayDeque<String> cardsDeque1 = new ArrayDeque<>(Arrays.asList(cards1));
+        ArrayDeque<String> cardsDeque2 = new ArrayDeque<>(Arrays.asList(cards2));
+        ArrayDeque<String> goalDeque = new ArrayDeque<>(Arrays.asList(goal));
 
+        while (!goalDeque.isEmpty()) {
+            if(!cardsDeque1.isEmpty() && cardsDeque1.peekFirst().equals(goalDeque.peek())) {
+                cardsDeque1.pollFirst();
+                goalDeque.pollFirst();
+            } else if (!cardsDeque2.isEmpty() && cardsDeque2.peekFirst().equals(goalDeque.peek())) {
+                cardsDeque2.pollFirst();
+                goalDeque.pollFirst();
+            } else {
+                break;
+            }
+        }
+        return goalDeque.isEmpty() ? "Yes" : "No";
     }
 
 }
