d4cb5c7bbe122bf34a8afd87507c5d44ac986cdb
NaMinhyeok <nmh9097@gmail.com>
2025-04-24 23:33:06 +0900

    21번 hashmap

--- 코딩테스트합격자되기/21.java
+++ 코딩테스트합격자되기/21.java
@@ -4,7 +4,28 @@ import java.util.HashMap;
 public class Solution {
 
     private String[] solution(String[] record) {
+        HashMap<String, String> msg = new HashMap<>();
+        msg.put("Enter", "님이 들어왔습니다.");
+        msg.put("Leave", "님이 나갔습니다.");
 
+        HashMap<String, String> uid = new HashMap<>();
+
+        for(String s : record) {
+            String[] cmd = s.split(" ");
+            if (cmd.length == 3) {
+                uid.put(cmd[1], cmd[2]);
+            }
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+        for(String s : record) {
+            String[] cmd = s.split(" ");
+            if (msg.containsKey(cmd[0])) {
+                result.add(uid.get(cmd[1]) + msg.get(cmd[0]));
+            }
+        }
+
+        return result.toArray(new String[0]);
     }
 
 }
