2aa0ca3827814cc059a651dcd27ffcfea174a4e7
NaMinhyeok <nmh9097@gmail.com>
2025-04-29 23:46:23 +0900

    23번 hashmap

--- 코딩테스트합격자되기/23.java
+++ 코딩테스트합격자되기/23.java
@@ -5,7 +5,34 @@ import java.util.Map;
 public class Solution {
 
     public int[] solution(String[] id_list, String[] report, int k) {
+        HashMap<String, HashSet<String>> reportedUser = new HashMap<>();
+        HashMap<String, Integer> count = new HashMap<>();
 
+        for (String r: report) {
+            String[] split = r.split(" ");
+            String userId = split[0];
+            String reportedId = split[1];
+
+            if(!reportedUser.containsKey(reportedId)) {
+                reportedUser.put(reportedId, new HashSet<>());
+            }
+            reportedUser.get(reportedId).add(userId);
+        }
+
+        for(Map.Entry<String, HashSet<String>> entry: reportedUser.entrySet()) {
+            if(entry.getValue().size() >= k) {
+                for(String userId: entry.getValue()) {
+                    count.put(userId, count.getOrDefault(userId, 0) + 1);
+                }
+            }
+        }
+
+        int[] answer = new int[id_list.length];
+
+        for (int i=0; i<id_list.length; i++) {
+            answer[i] = count.getOrDefault(id_list[i], 0);
+        }
+        return answer;
     }
 
 }
