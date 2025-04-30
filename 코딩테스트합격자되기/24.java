251640151c94b812e2b2fdd662db093dcaf03bad
NaMinhyeok <nmh9097@gmail.com>
2025-04-30 23:12:16 +0900

    24번 hashmap

--- 코딩테스트합격자되기/24.java
+++ 코딩테스트합격자되기/24.java
@@ -2,8 +2,46 @@ import java.util.*;
 
 public class Solution {
 
-    public String[] solution(String[] orders, int[] course) {
+    private static HashMap<Integer,HashMap<String,Integer>> courseMap;
 
+    public String[] solution(String[] orders, int[] course) {
+        courseMap = new HashMap<>();
+        for(int i: course) {
+            courseMap.put(i,new HashMap<>());
+        }
+
+        for(String order: orders) {
+            char[] orderArray = order.toCharArray();
+            Arrays.sort(orderArray);
+            comibination(0, orderArray, "");
+        }
+
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(HashMap<String,Integer> count: courseMap.values()) {
+            count.values()
+                .stream()
+                .max(Comparator.comparingInt(o -> o))
+                .ifPresent(cnt -> count.entrySet()
+                    .stream()
+                    .filter(entry -> count.equals(entry.getValue()) && cnt > 1)
+                    .forEach(entry -> answer.add(entry.getKey())));
+        }
+
+        Collections.sort(answer);
+        return answer.toArray(new String[0]);
     }
+
+    private static void comibination(int idx, char[] order, String result) {
+        if(courseMap.containsKey(result.length())) {
+            HashMap<String, Integer> map = courseMap.get(result.length());
+            map.put(result, map.getOrDefault(result, 0) + 1);
+        }
+
+        for(int i = idx; i<order.length; i++) {
+            comibination(i+1, order, result + order[i]);
+        }
+    }
+
 
 }
