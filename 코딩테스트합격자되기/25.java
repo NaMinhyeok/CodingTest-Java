bf9afee3b4d6d6969ccc439b01e3c201983f896c
NaMinhyeok <nmh9097@gmail.com>
2025-05-01 23:08:57 +0900

    25번 tree

--- 코딩테스트합격자되기/25.java
+++ 코딩테스트합격자되기/25.java
@@ -9,7 +9,32 @@ public class Solution {
 
     // 이 부분을 변경해서 실행해보세요.
     public static String[] solution(int[] nodes) {
+        String[] result = new String[3];
+        result[0] = preorder(nodes, 0).trim();
+        result[1] = inorder(nodes, 0).trim();
+        result[2] = postorder(nodes, 0).trim();
+        return result;
+    }
 
+    private static String postorder(int[] nodes, int idx) {
+        if (idx >= nodes.length) {
+            return "";
+        }
+        return postorder(nodes, 2 * idx + 1) + postorder(nodes, 2 * idx + 2) + nodes[idx];
+    }
+
+    private static String inorder(int[] nodes, int idx) {
+        if (idx >= nodes.length) {
+            return "";
+        }
+        return inorder(nodes, 2 * idx + 1) + nodes[idx] + inorder(nodes, 2 * idx + 2);
+    }
+
+    private static String preorder(int[] nodes, int idx) {
+        if (idx >= nodes.length) {
+            return "";
+        }
+        return nodes[idx] + "" + preorder(nodes, 2 * idx + 1) + preorder(nodes, 2 * idx + 2);
     }
 
 }
