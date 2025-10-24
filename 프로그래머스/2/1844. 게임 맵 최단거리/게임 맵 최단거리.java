62ff9340a883c8791fc03ffdd598f3f26df55974
Na Minhyeok <112960401+NaMinhyeok@users.noreply.github.com>
2025-10-24 19:45:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.88 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,37 +3,47 @@ import java.util.*;
 class Solution {
     static int[] dx = {0,1,0,-1};
     static int[] dy = {1,0,-1,0};
-    public int solution(int[][] maps) {
-        int answer = 0;
-        boolean ans;
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        BFS(maps,visited);
-        if(!visited[maps.length-1][maps[0].length-1]){
-            return -1;
-        } else {
-            return maps[maps.length-1][maps[0].length-1];
+    
+    static class Node {
+        int x,y;
+        
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
-    private static void BFS(int[][] maps, boolean[][] visited){
-        int x=0;
-        int y=0;
-        visited[x][y] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {x,y});
+    
+    public int solution(int[][] maps) {
+        ArrayDeque<Node> queue = new ArrayDeque<>();
+        int n = maps.length;
+        int m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        int[][] dist = new int[n][m];
         
-        while(!queue.isEmpty()){
-            int[] now = queue.poll();
-            for(int i=0; i<4; i++){
-                int nowx = now[0] + dx[i];
-                int nowy = now[1] + dy[i];
-                if(nowx>=0 && nowy>=0 && nowx<maps.length && nowy<maps[0].length){
-                    if(!visited[nowx][nowy] && maps[nowx][nowy]!=0){
-                        maps[nowx][nowy] = maps[now[0]][now[1]]+1;
-                        visited[nowx][nowy] = true;
-                        queue.add(new int[] {nowx,nowy});
-                    }
+        dist[0][0] = 1;
+        queue.addLast(new Node(0,0));
+        visited[0][0] = true;
+        while(!queue.isEmpty()) {
+            Node now = queue.pollFirst();
+            for(int i=0; i<4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m ) {
+                    continue;
+                }
+                if(maps[nx][ny] == 0) {
+                    continue;
+                }
+                
+                if(visited[nx][ny] == false) {
+                	visited[nx][ny] = true;    
+                    queue.addLast(new Node(nx,ny));
+                    dist[nx][ny] = dist[now.x][now.y] + 1;
                 }
             }
         }
+        
+        return dist[n-1][m-1] != 0 ? dist[n-1][m-1] : -1;
     }
+    
 }
