10eb3c6ee498c3a4fb56056163569131069a86ed
Na Minhyeok <112960401+NaMinhyeok@users.noreply.github.com>
2025-12-15 23:41:21 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 631.78 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        int n = elements.length;
+        int[] doubleElements = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            doubleElements[i] = elements[i];
+            doubleElements[i + n] = elements[i];
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {   
+            for (int j = i; j < i + n; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += doubleElements[k];
+                }
+                set.add(sum);
+            }
+        }
+        return set.size();
+    }
+}
